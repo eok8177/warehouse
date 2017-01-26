ce14715782262f3eeb2c4103f42b5469f74f8c4a
Evgen Koromyslov <eok8177@gmail.com>
2017-01-26 20:43:24 +0200

    translates

--- resources/views/backend/sidebar.blade.php
+++ resources/views/backend/sidebar.blade.php
@@ -1,6 +1,6 @@
 <h3>Left menu</h3>
 <ul class="nav side-nav">
   <li><a href="{{ route('backend.dashboard') }}">Dashboard</a></li>
-  <li><a href="{{ route('backend.lpz.index') }}">Lpz</a></li>
-  <li><a href="{{ route('backend.work.index') }}">Work</a></li>
+  <li><a href="{{ route('backend.lpz.index') }}">@lang('messages.lpz')</a></li>
+  <li><a href="{{ route('backend.work.index') }}">@lang('messages.work')</a></li>
 </ul>
